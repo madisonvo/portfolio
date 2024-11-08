208657fb72c1e6678460b037ca2f58a33fc47303
Madison Vo <madisonvo10@gmail.com>
2024-11-08 12:55:24 -0700

    irrelevant changes

--- timesheet-management/spring-boot-timesheet/src/main/java/com/timesheet_management/spring_boot_timesheet/model/Holiday.java
+++ timesheet-management/spring-boot-timesheet/src/main/java/com/timesheet_management/spring_boot_timesheet/model/Holiday.java
@@ -40,6 +40,6 @@ public class Holiday {
     }
 
     public void setHolidayDate(Date holidayDate) {
-        this.holidayDate = this.holidayDate;
+        this.holidayDate = holidayDate;
     }
 }
--- timesheet-management/spring-boot-timesheet/src/main/java/com/timesheet_management/spring_boot_timesheet/service/HolidayService.java
+++ timesheet-management/spring-boot-timesheet/src/main/java/com/timesheet_management/spring_boot_timesheet/service/HolidayService.java
@@ -1,6 +1,5 @@
 package com.timesheet_management.spring_boot_timesheet.service;
 
-import com.timesheet_management.spring_boot_timesheet.repository.HolidayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
